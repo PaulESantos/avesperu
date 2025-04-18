569fbbac7f5dfa0e7929d3d9e41d2a6680b27b0b
PaulESantos <paulefrens@gmail.com>
2025-04-17 22:12:17 -0500

    update check

--- data-raw/build_table.R
+++ data-raw/build_table.R
@@ -2,79 +2,81 @@
 library(pdftools)
 library(tidyverse)
 
-text <- pdf_text("List of the Birds of Peru 2025 01.pdf" )
+text <- pdf_text("List of the Birds of Peru 2025 02.pdf" )
 
 length(text)
 
 get_aves_tab <- function(text){
   text_ <- text |>
-    str_split("\r|\n") |>
+    stringr::str_split("\r|\n") |>
     unlist()
 
   text_ |>
-    enframe() |>
-    mutate(value = str_trim(value)) |>  # Elimina espacios en blanco al inicio y al final
-    filter(nchar(value) > 0) |>
-    separate(value,
+    tibble::enframe() |>
+    dplyr::mutate(value = stringr::str_trim(value)) |>  # Elimina espacios en blanco al inicio y al final
+    dplyr::filter(nchar(value) > 0) |>
+    tidyr::separate(value,
              into = c("scientific_name", "english_name", "spanish_name"), # Cambiado el orden de columnas
              sep = "\\s{2,}",            # Divide por al menos dos espacios consecutivos
              extra = "merge",            # Une valores adicionales si hay más de 3 columnas
              fill = "right",
              remove = FALSE)    |>
-    select(-c(1:2)) |>
-    mutate(family_name =
-             if_else(
-               str_detect(scientific_name, "F[a-z]{1,} [A-Z]{1,}"),
+    dplyr::select(-c(1:2)) |>
+    dplyr::mutate(family_name =
+                    dplyr::if_else(
+                    stringr::str_detect(scientific_name, "F[a-z]{1,} [A-Z]{1,}"),
                scientific_name,
                NA_character_
              )) |>
-    mutate(order_name =
-             if_else(
-               str_detect(scientific_name, "O[a-z]{1,} [A-Z]{1,}"),
+    dplyr::mutate(order_name =
+                    dplyr::if_else(
+               stringr::str_detect(scientific_name, "O[a-z]{1,} [A-Z]{1,}"),
                scientific_name,
                NA_character_
              )) |>
-    mutate(status = case_when(
-      str_detect(scientific_name, "\\(.*?\\)")~
-        str_extract(scientific_name,
+    dplyr::mutate(status = dplyr::case_when(
+      stringr::str_detect(scientific_name, "\\(.*?\\)")~
+        stringr::str_extract(scientific_name,
                     "(?<=\\().*?(?=\\))"),
       TRUE ~ "X"
     )
     ) |>
-    fill(family_name, .direction = "down") |>
-    fill(order_name, .direction = "down") |>
-    relocate(order_name, family_name, everything()) |>
-    filter(#!is.na(family_name),
+    tidyr::fill(family_name, .direction = "down") |>
+    tidyr::fill(order_name, .direction = "down") |>
+    dplyr::relocate(order_name, family_name, dplyr::everything()) |>
+    dplyr::filter(#!is.na(family_name),
            !is.na(spanish_name)) |>
-    mutate(
-      order_name = str_extract(order_name, "\\b[A-Z]+\\b") |>
-        str_to_sentence(),
-      family_name = str_extract(family_name, "\\b[A-Z]+\\b") |>
-        str_to_sentence()
+    dplyr::mutate(
+      order_name = stringr::str_extract(order_name, "\\b[A-Z]+\\b") |>
+        stringr::str_to_sentence(),
+      family_name = stringr::str_extract(family_name, "\\b[A-Z]+\\b") |>
+        stringr::str_to_sentence()
     ) |>
     #  select(scientific_name, categoria) |>
-    mutate(scientific_name = case_when(
-      str_detect(scientific_name, "\\(.*?\\)") ~
-        str_remove(scientific_name, "\\(.*?\\)") |>  str_trim() |>  str_squish(),
+    dplyr::mutate(scientific_name = dplyr::case_when(
+      stringr::str_detect(scientific_name, "\\(.*?\\)") ~
+        stringr::str_remove(scientific_name, "\\(.*?\\)") |>
+        stringr::str_trim() |>
+        stringr::str_squish(),
       TRUE ~ scientific_name
     ))
 
 }
 
-aves_peru_2025_1 <- map_dfr(text[1:29],
+aves_peru_2025_2 <- purrr::map_dfr(text[1:34],
                             ~ get_aves_tab(.))
 
-aves_peru_2025_v1 <- aves_peru_2025_1 |>
-  fill(family_name, .direction = "down") |>
-  fill(order_name, .direction = "down") |>
-  filter(!is.na(order_name),
+aves_peru_2025_v2 <- aves_peru_2025_2 |>
+  tidyr::fill(family_name, .direction = "down") |>
+  tidyr::fill(order_name, .direction = "down") |>
+  dplyr::filter(!is.na(order_name),
          !is.na(family_name))
 
-aves_peru_2025_v1
+aves_peru_2025_v2
 
 
-aves_peru_2025_v1 |>
-  distinct(status)
+aves_peru_2025_v2 |>
+  dplyr::distinct(status)
 
 
 #' (E) = endémico; una especie es considerada endémica para Perú hasta que un registro fuera de sus fronteras ha sido publicado.
@@ -97,20 +99,13 @@ categoria_map <- c(
 )
 length(categoria_map)
 
+aves_peru_2025_v2 <- aves_peru_2025_v2 |>
+  dplyr::mutate(status = dplyr::recode(status, !!!categoria_map)) |>
+  dplyr::mutate(status = stringr::str_to_sentence(status))
 
-
-
-aves_peru_2025_v1 <- aves_peru_2025_v1 |>
-  mutate(status = recode(status, !!!categoria_map)) |>
-  mutate(status = str_to_sentence(status))
-
-aves_peru_2025_v1 |>
-  count(status) |>
+aves_peru_2025_v2 |>
+  dplyr::count(status) |>
   janitor::adorn_totals()
-
-
-aves_peru_2025_v1 |>
-  count(status)
 
 # 2024 v2
 #1#  X = residente:      1542 v
@@ -129,9 +124,16 @@ aves_peru_2025_v1 |>
 # IN = introducido:   3
 # EX = extirpado:     0
 # H = hipotético:     26
+#################################
+# 2025 - v2
+# X = residente:      1542
+# E = endémico:       119
+# NB = migratorio:    138
+# V = divagante:      84
+# IN = introducido:   3
+# EX = extirpado:     0
+# H = hipotético:     26
 
-
-
-usethis::use_data(aves_peru_2025_v1,
+usethis::use_data(aves_peru_2025_v2,
                   compress = "xz",
                   overwrite = TRUE)
